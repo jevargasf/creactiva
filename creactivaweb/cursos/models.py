dab904c814d21305f3e917c18385dcc9b96f0f5a
Javier <jevargasf@gmail.com>
2024-12-03 22:14:05 -0300

    Nombres modelos panel admin django

--- creactivaweb/cursos/models.py
+++ creactivaweb/cursos/models.py
@@ -1,39 +1,54 @@
 from django.db import models
 
 class Etiqueta(models.Model):
-    id_eti = models.AutoField(primary_key=True, null=False)
-    nom_eti = models.CharField(max_length=50)
+    id_eti = models.AutoField(primary_key=True, null=False, verbose_name='ID Etiqueta')
+    nom_eti = models.CharField(max_length=50, verbose_name='Nombre etiqueta')
+
+    def __str__(self):
+        return f'{self.nom_eti}'
 
 class MaterialComplementario(models.Model):
-    id_mat = models.AutoField(primary_key=True, null=False)
-    nom_mat = models.CharField(max_length=255)
-    lin_mat = models.URLField()
+    id_mat = models.AutoField(primary_key=True, null=False, verbose_name='ID material')
+    nom_mat = models.CharField(max_length=255, verbose_name='Nombre material')
+    lin_mat = models.URLField(verbose_name='Link material')
+
+    def __str__(self):
+        return f'{self.nom_mat}'
 
 class Capitulo(models.Model):
-    id_cap = models.AutoField(primary_key=True, null=False)
-    nom_cap = models.CharField(max_length=255)
-    des_cap = models.TextField()
-    lin_cap = models.URLField()
+    id_cap = models.AutoField(primary_key=True, null=False, verbose_name='ID capítulo')
+    num_cap = models.IntegerField(null=True, blank=True, verbose_name='Número capítulo')
+    nom_cap = models.CharField(max_length=255, verbose_name='Nombre capítulo')
+    des_cap = models.TextField(verbose_name='Descripción')
+    lin_cap = models.URLField(verbose_name='Link capítulo')
     id_lin = models.ForeignKey(
         MaterialComplementario,
         blank=True, 
         null=True,
         on_delete=models.CASCADE,
-        related_name='materiales'
+        related_name='materiales',
+        verbose_name='Material asociado'
     )
 
+    def __str__(self):
+        return f'Número: {self.num_cap} | {self.nom_cap}'
+
 class Curso(models.Model):
-    id_cur = models.AutoField(primary_key=True, null=False)
-    nom_cur = models.CharField(max_length=255)
-    des_cur = models.TextField()
+    id_cur = models.AutoField(primary_key=True, null=False, verbose_name='ID curso')
+    nom_cur = models.CharField(max_length=255, verbose_name='Nombre curso')
+    des_cur = models.TextField(verbose_name='Descripción')
     id_eti = models.ForeignKey(
         Etiqueta,
         on_delete=models.CASCADE,
-        related_name='etiquetas'
+        related_name='etiquetas',
+        verbose_name='Etiqueta'
     )
     id_cap = models.ForeignKey(
         Capitulo,
         on_delete=models.CASCADE,
-        related_name='capitulos'
+        related_name='capitulos',
+        verbose_name='Capítulo'
     )
 
+    def __str__(self):
+        return f'{self.nom_cur}'
--- creactivaweb/main/models.py
+++ creactivaweb/main/models.py
@@ -2,19 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Perfil(models.Model):
-    codigo_perfil = models.CharField(max_length=3, null=False, blank=False)
-    comuna = models.CharField(max_length=255, null=True, blank=True)
-    region = models.CharField(max_length=255, null=True, blank=True)
-    telefono = models.CharField(max_length=12, null=True, blank=True)
+    cod_per = models.CharField(max_length=3, null=False, blank=False, default='100', verbose_name='Código perfil')
+    com_per = models.CharField(max_length=255, null=True, blank=True, verbose_name='Comuna')
+    reg_per = models.CharField(max_length=255, null=True, blank=True, verbose_name='Región')
+    tel_per = models.CharField(max_length=12, null=True, blank=True, verbose_name='Teléfono')
     user = models.OneToOneField(
         User,
         related_name='perfil',
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        verbose_name='Usuario'
     )
 
     def __str__(self):
         nombre = self.user.first_name
         apellido = self.user.last_name
         usuario = self.user.email
-        tipo = self.codigo_perfil
-        return f'{nombre} {apellido} | {usuario} | {tipo}'
+        tipo = self.cod_per
+        return f'{nombre} {apellido} | Username: {usuario} | Código: {tipo}'
