6929420c1597a790e8ff873f193517ab05860a56
Javier <jevargasf@gmail.com>
2024-12-08 19:32:31 -0300

    producción manual

--- creactivaweb/creactivaweb/settings.py
+++ creactivaweb/creactivaweb/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'creactivaweb.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
 
@@ -128,7 +128,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
     os.path.join(BASE_DIR, 'static'),
     #"/var/www/static/",
 ]
