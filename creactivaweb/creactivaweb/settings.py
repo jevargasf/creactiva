453c460afb7a398bf93db32742c995dc32751c80
Javier <jevargasf@gmail.com>
2024-12-08 23:17:38 -0300

    producción

--- creactivaweb/creactivaweb/settings.py
+++ creactivaweb/creactivaweb/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.creactivaanimaciones.cl']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -128,11 +128,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    #"/var/www/static/",
+    os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = '/home/creacti3/public_html/static/'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
