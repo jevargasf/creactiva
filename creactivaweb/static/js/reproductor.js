ccc7a5ffe069eae31846151854f8aa829e0490d4
Javier <jevargasf@gmail.com>
2024-12-22 13:03:39 -0300

    ejecución asíncrona de reproductor.js

--- creactivaweb/static/js/reproductor.js
+++ creactivaweb/static/js/reproductor.js
@@ -1,3 +1,13 @@
 // Ante cualquier cambio, subir este código al hosting
-boton_play = document.getElementsByClassName("video-click-to-play ui play-button-overlay-glyph")
-boton_play[0].innerHTML = 'Comenzar'
+$.getScript( "https://cdn.cloud.techsmith.com/smartplayer/5/latest/techsmith-smart-player.min.js" )
+  .done(function( script, textStatus ) {
+    console.log( textStatus );
+    boton_play = document.getElementsByClassName("video-click-to-play ui play-button-overlay-glyph")
+    console.log(boton_play[0])
+    boton_play[0].innerHTML = 'Comenzar'
+  })
+  .fail(function( jqxhr, settings, exception ) {
+    //script fail warning if you want it
+    console.log("Algo falló")
+});
+
