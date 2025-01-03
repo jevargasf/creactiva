1e497523597d67a0bf05cb01f9d77c898dce4ef6
Javier <jevargasf@gmail.com>
2025-01-03 16:55:03 -0300

    página cursos con información desde el backend

--- creactivaweb/cursos/models.py
+++ creactivaweb/cursos/models.py
@@ -3,32 +3,32 @@ from django.db import models
 
 class Etiqueta(models.Model):
     eti = models.AutoField(primary_key=True, null=False, verbose_name='ID Etiqueta')
-    nom_eti = models.CharField(max_length=50, verbose_name='Nombre etiqueta')
+    nombre = models.CharField(max_length=50, verbose_name='Nombre etiqueta')
 
     def __str__(self):
-        return self.nom_eti
+        return self.nombre
 
 class Idioma(models.Model):
     idi = models.AutoField(primary_key=True, null=False, verbose_name='ID idioma')
-    nom_idi = models.CharField(max_length=255, verbose_name='Nombre idioma')
+    nombre = models.CharField(max_length=255, verbose_name='Nombre idioma')
 
     def __str__(self):
-        return self.nom_idi
+        return self.nombre
 
 class Autor(models.Model):
     aut = models.AutoField(primary_key=True, null=False, verbose_name='ID autor')
-    nom_aut = models.CharField(max_length=255, verbose_name='Nombre autor')
+    nombre_autor = models.CharField(max_length=255, verbose_name='Nombre autor')
 
     def __str__(self):
-        return self.nom_aut
+        return self.nombre_autor
 
 class MaterialesComplementario(models.Model):
     mat = models.AutoField(primary_key=True, null=False, verbose_name='ID material')
-    nom_mat = models.CharField(max_length=255, verbose_name='Nombre material')
-    lin_mat = models.URLField(verbose_name='Link material')
+    nombre_material = models.CharField(max_length=255, verbose_name='Nombre material')
+    link_material = models.CharField(max_length=255, null=True, verbose_name='Link material')
 
     def __str__(self):
-        return f'{self.nom_mat}'
+        return f'{self.nombre_material}'
 
 class Curso(models.Model):
     cur = models.AutoField(primary_key=True, null=False, verbose_name='ID curso')
@@ -49,23 +49,23 @@ class Curso(models.Model):
         verbose_name='Idioma',
         default=1
     )
-    aut = models.ManyToManyField(Autor, verbose_name='Autores', related_name='autores')
+    autores = models.ManyToManyField(Autor, verbose_name='Autores', related_name='autores')
 
     def __str__(self):
         return self.nombre
 
 class Capitulo(models.Model):
     cap = models.AutoField(primary_key=True, null=False, verbose_name='ID capítulo')
-    nom_cap = models.CharField(max_length=255, verbose_name='Nombre capítulo')
-    dur_cap = models.IntegerField(verbose_name='Duración (minutos)')
-    des_cap = models.TextField(verbose_name='Descripción')
-    nin_cap = models.IntegerField(verbose_name='Número de actividades interactivas')
-    lin_cap = models.URLField(verbose_name='Link capítulo', max_length=255)
+    nombre = models.CharField(max_length=255, verbose_name='Nombre capítulo')
+    duracion = models.IntegerField(verbose_name='Duración (minutos)')
+    descripcion = models.TextField(verbose_name='Descripción')
+    num_actividades = models.IntegerField(verbose_name='Número de actividades interactivas')
+    link = models.URLField(verbose_name='Link capítulo', max_length=255)
     xml_cap = models.CharField(verbose_name='Link xml', null=True, max_length=255)
     js_cap = models.CharField(verbose_name='Link xml.js', null=True, max_length=255)
-    thu_cap = models.CharField(verbose_name='Link thumbnail', null=True, max_length=255)
-    fir_cap = models.CharField(verbose_name='Link frame preview', null=True, max_length=255)
-    mat = models.ForeignKey(
+    thumbnail = models.CharField(verbose_name='Link thumbnail', null=True, max_length=255)
+    first_frame = models.CharField(verbose_name='Link frame preview', null=True, max_length=255)
+    material = models.ForeignKey(
         MaterialesComplementario,
         blank=True, 
         null=True,
@@ -82,12 +82,12 @@ class Capitulo(models.Model):
     )
 
     def __str__(self):
-        return self.nom_cap
+        return self.nombre
 
     
     
 class EstadoCapitulo(models.Model):
-    est_cap = models.AutoField(primary_key=True, null=False, verbose_name='ID estado capítulo')
+    est = models.AutoField(primary_key=True, null=False, verbose_name='ID estado capítulo')
     perfil = models.ForeignKey(
         'main.Perfil',
         on_delete=models.CASCADE,
@@ -119,8 +119,8 @@ class Visualizacion(models.Model):
         verbose_name='capitulo',
         related_name='capitulo_vis'
     )
-    fec_vis = models.DateTimeField(null=False, verbose_name='Última visualización')
-    min_vis = models.IntegerField(null=False, verbose_name='Minuto última visualización')
+    fecha = models.DateTimeField(null=False, verbose_name='Última visualización')
+    minuto = models.IntegerField(null=False, verbose_name='Minuto última visualización')
 
     def __str__(self):
-        return f'{self.perfil.user} {self.fec_vis}'
+        return f'{self.perfil.user} {self.fecha}'
--- creactivaweb/cursos/views.py
+++ creactivaweb/cursos/views.py
@@ -13,12 +13,23 @@ class CursoView(View):
     def get (self, request, id):
         curso = Curso.objects.get(pk=id)
         capitulos = pedir_capitulos(id)
+        duracion_curso = 0
+        actividades = 0
+        num_materiales = 0
+        for capitulo in capitulos:
+            duracion_curso += capitulo.duracion
+            actividades += capitulo.num_actividades
+            if capitulo.material == None:
+                continue
+            else:
+                num_materiales += 1
         context = {
             'curso': curso,
+            'duracion': round(duracion_curso/60),
+            'actividades': actividades,
+            'num_materiales': num_materiales,
             'capitulos': capitulos
         }
-        print(curso)
-        print(capitulos)
         return render(request, 'curso.html', context)
     
 
@@ -26,10 +37,16 @@ class CapituloView(View):
     def dispatch(self, *args, **kwargs):
         return super().dispatch(*args, **kwargs)
     
-    def get (self, request: HttpRequest):
-        print(request.get_full_path_info)
+    def get (self, request: HttpRequest, id_curso):
+        curso = Curso.objects.get(pk=id_curso)
+        context = {
+            'curso': curso
+        }
         if request.user.is_authenticated == True:
-            return render(request, 'reproductor.html')
+            # content_params = {
+            #     'param': 1
+            # }
+            return render(request, 'reproductor.html', context)
         else:
             return render(request, 'registration/login.html')
         
