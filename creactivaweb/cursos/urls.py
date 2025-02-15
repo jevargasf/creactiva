8d11d299eac306622504ff43f01884377ff62df6
Javier <jevargasf@gmail.com>
2025-02-15 20:23:12 -0300

    debug recuperar último minuto visualización cuando cierra sesión

--- creactivaweb/cursos/urls.py
+++ creactivaweb/cursos/urls.py
@@ -3,7 +3,7 @@ from cursos.views import CursoView, CapituloView, TrailerView
 
 urlpatterns = [
     path('cursos/<id>', CursoView.as_view(), name='cursos'),
-    path('trailer/<id>', TrailerView.as_view(), name='trailer'),
+    # path('trailer/<id>', TrailerView.as_view(), name='trailer'),
     path('capitulo/<id>', CapituloView.as_view(), name='capitulo'),
 ]
 # <int:id_curso>/<str:embedIFrameId>
--- creactivaweb/cursos/views.py
+++ creactivaweb/cursos/views.py
@@ -8,6 +8,7 @@ from main.models import User, Perfil
 from django.utils import timezone
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
+import pyjson5, json
 
 
 class CursoView(View):
@@ -15,7 +16,9 @@ class CursoView(View):
         return super().dispatch(*args, **kwargs)
     
     def get (self, request, id):
+        print("PIDO ESTA DATA")
         context = data_curso(id)
+        print(context['capitulos'][0].cap)
         return render(request, 'curso.html', context)
     
 class TrailerView(View):
@@ -32,21 +35,22 @@ class CapituloView(View):
         return super().dispatch(*args, **kwargs)
     def get (self, request: HttpRequest, id):
         capitulo = Capitulo.objects.get(pk=id)
+        context = {
+            'info': {
+                'capitulo_nombre': capitulo.nombre,
+                'curso_nombre': capitulo.curso.nombre,
+                'curso_id': capitulo.curso.cur,
+                'desc_corta': capitulo.desc_corta,
+                'numero': capitulo.numero,
+                'descripcion': capitulo.descripcion,
+                'duracion': capitulo.duracion,
+                'first_frame': capitulo.first_frame,
+                'contenidos': capitulo.contenidos,
+                'first_frame': capitulo.first_frame
+            },
+            'recurso': {}
+        }
         if request.user.is_authenticated:
-            context = {
-                'info': {
-                    'capitulo_nombre': capitulo.nombre,
-                    'curso_nombre': capitulo.curso.nombre,
-                    'curso_id': capitulo.curso.cur,
-                    'desc_corta': capitulo.desc_corta,
-                    'numero': capitulo.numero,
-                    'descripcion': capitulo.descripcion,
-                    'duracion': capitulo.duracion,
-                    'first_frame': capitulo.first_frame,
-                    'contenidos': capitulo.contenidos
-                },
-                'recurso': {}
-            }
                 # TIENE CUENTA      
             perfil_object = pedir_perfil(request.user)
             # TIENE SUSCRIPCIÓN INDIVIDUAL
@@ -54,7 +58,6 @@ class CapituloView(View):
                 context['recurso']['js_cap'] = capitulo.js_cap
                 context['recurso']['link'] = capitulo.link
                 context['recurso']['xml_cap'] = capitulo.xml_cap
-                context['recurso']['first_frame'] = capitulo.first_frame
                 ultima_visualizacion = pedir_ultima_visualizacion(perfil_object, capitulo)
                 if ultima_visualizacion != None:
                     context['minuto'] = ultima_visualizacion
@@ -62,25 +65,35 @@ class CapituloView(View):
                 return render(request, 'reproductor.html', context)
             # NO TIENE SUSCRIPCIÓN INDIVIDUAL, MANDA INFO TRAILER EN VEZ DE MANDAR CONTEXT, MEJO REDIRECT A URL TRAILER
             else:
-                context = data_curso(capitulo.curso.cur)
-                return redirect('trailer', capitulo.curso.cur)
+                context['recurso']['js_trailer'] = capitulo.curso.js_trailer
+                context['recurso']['xml_trailer'] = capitulo.curso.xml_trailer
+                context['recurso']['link'] = capitulo.curso.link_trailer
+                # DATA CAP + TRAILER
+                return render(request, 'trailer.html', context)
         else:
-            context = data_curso(capitulo.curso.cur)
-            return redirect('trailer', capitulo.curso.cur)
+            context['recurso']['js_cap'] = capitulo.curso.js_trailer
+            context['recurso']['xml_cap'] = capitulo.curso.xml_trailer
+            context['recurso']['link'] = capitulo.curso.link_trailer
+            # DATA CAP + TRAILER
+            return render(request, 'trailer.html', context)
         
     def post(self, request: HttpRequest, id):
         # código para almacenar el segundo de reproducción
         # ver bien el evento que desencadenará la escritura de la bbdd
         # el document.visibility hidden puede que envíe demasidas peticiones
         # pero el unload o el beforeunload tienen más casos de fallo
-        res = request.body
-        t_string = res.decode('utf-8').replace('"','')
-        t_float = float(t_string)
+        res = json.loads(request.body)
+        # t_string = res['minuto'].replace('"','')
+        t_float = float(res['minuto'])
+        print(t_float)
         capitulo = Capitulo.objects.get(cap=id)
         # if t_reproduccion != null, almacenar, si no, continuar
         # guardo el correo, porque con eso puedo recuperar el código perfil y demás información
         # de cliente
-        perfil = Perfil.objects.get(user=request.user)
+        user_clean = res['user'].strip('"')
+        print(user_clean)
+        user_object = User.objects.get(username=user_clean)
+        perfil = Perfil.objects.get(user=user_object)
         visualizacion = Visualizacion(
             perfil=perfil,
             capitulo=capitulo,
