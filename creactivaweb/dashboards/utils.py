f22d3740939f510e7de82cbb98be40cf46c4033c
Javier <jevargasf@gmail.com>
2025-02-17 11:48:26 -0300

    debug perfilsuscripcion en utils dashboard

--- creactivaweb/dashboards/utils.py
+++ creactivaweb/dashboards/utils.py
@@ -6,19 +6,19 @@ def suscripcion_activa(user):
         user_object = User.objects.get(username=user)
         perfil_object = Perfil.objects.get(user_id=user_object.id)
         if perfil_object.codigo[0] == '1':
-            with PerfilSuscripcion.objects.get(perfil_id=perfil_object.id, estado_suscripcion='1') as registro_object: 
-                if registro_object:
-                    suscripcion_object = Suscripcion.objects.get(pk=registro_object.suscripcion_id, numero_usuarios=1)
-                    if suscripcion_object:
-                        return {
-                            'suscripcion':suscripcion_object, 
-                            'dias_restantes': (suscripcion_object.fecha_termino - now()).days, 
-                            'perfil': perfil_object
-                            }
-                    else:
-                        return {'perfil': perfil_object}
+            registro_object = PerfilSuscripcion.objects.get(perfil_id=perfil_object.id, estado_suscripcion='1')
+            if registro_object:
+                suscripcion_object = Suscripcion.objects.get(pk=registro_object.suscripcion_id, numero_usuarios=1)
+                if suscripcion_object:
+                    return {
+                        'suscripcion':suscripcion_object, 
+                        'dias_restantes': (suscripcion_object.fecha_termino - now()).days, 
+                        'perfil': perfil_object
+                        }
                 else:
                     return {'perfil': perfil_object}
+            else:
+                return {'perfil': perfil_object}
         else:
             return {'perfil': perfil_object}
     except Exception as e:
--- creactivaweb/formato.py
+++ creactivaweb/formato.py
@@ -1,8 +1,3 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
 def separador_miles(valor):
     try:
         valor_str = str(valor)
@@ -10,6 +5,7 @@ def separador_miles(valor):
         str_reversed = valor_str[::-1]
         for x, a in enumerate(str_reversed):
             if (x+2)%3 == 0:
+                print(a)
                 str_result += str_reversed[x]
                 str_result += "."
             else:
@@ -17,3 +13,6 @@ def separador_miles(valor):
         return str_result
     except (ValueError, TypeError):
         return valor
+    
+print(separador_miles(12000))
+print(separador_miles(8333))
