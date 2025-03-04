c7ab0840efed58ca2ef20f182a407b44a3e12369
jevargasf <jevargasf@gmail.com>
2025-03-04 15:39:32 -0300

    alerta en primera carga index

--- creactivaweb/static/js/index.js
+++ creactivaweb/static/js/index.js
@@ -1,8 +1,23 @@
 
-console.log("COMENZÓ LA EJECUCIÓN NDEX")
 window.addEventListener('load', function () {
       $('body').css('overflow-y', 'auto');
       document.querySelector('#btn-spotlight1').classList.add('collapsed');
       document.querySelector('#slider-inicial-descripcion-1').classList.add('collapsed');
     //   document.querySelector('#add-favoritos').classList.add('collapsed');
   })
+
+// LOCAL STORAGE
+flag = localStorage.getItem("alert")
+if (localStorage.flag === "true"){
+  console.log("no hago nada")
+} else if (flag === null){
+  document.querySelector("#publicidad").innerHTML += 
+  `<div id="alerta" class="alert" role="alert">
+  <div>
+  <strong>USTED HA INGRESADO POR PRIMERA VEZ</strong>
+  <p>haz click para continuar</p>
+  <button id="cerrarAlerta"  type="button"><img src="" alt=""></button>
+  </div>
+  </div>`
+  localStorage.setItem("flag", "true")
+}
